517fe51d11f6757be53707193a121a25cc67543b
Nirmal Kumar <nirmalkumargurajada@gmail.com>
2022-09-29 00:23:22 +0530

    integrations

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -7,11 +7,12 @@ import Productivity from "../Pages/Blogs/Productivity"
 import RemoteWork from "../Pages/Blogs/RemoteWork"
 import Resource from "../Pages/Blogs/Resource"
 import HomePage from "../Pages/Home/HomePage"
-
+import Integration from "../Pages/Integrations/Integration"
 export const AllRoutes = () => {
     return <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path="/blog" element={<BlogPage />} /> 
+        <Route path="/integrations" element={<Integration/>} />
         <Route path="/productivity" element={<Productivity/>} />
         <Route path="/remotework" element={<RemoteWork/>} />
         <Route path="/practice" element={<BestPractice/>} />
