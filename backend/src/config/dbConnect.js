d8194101302d9e39758e67d28fef3cea5bbf874a
apoorvachikara <chikara02apoorva@gmail.com>
2022-11-04 17:42:41 +0530

    Don't put sensitive information to git
    feeback: fw18_0042 and fw16_644 -  don't put sensitive information directly into the code
    As you push to the github it is visible to all the people who have access to this repo

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+// feeback: fw18_0042 and fw16_644 -  don't put sensitive information directly into the code
+// As you push to the github it is visible to all the people who have access to this repo
 const connect = ()=>{
     return mongoose.connect("mongodb+srv://newTimeTracking:time1234@cluster0.zevrlzo.mongodb.net/newTimeTracking");
 }
