2d77cf7ee04471c8cd4bd2ca8e01f1a539a7e404
apoorvachikara <chikara02apoorva@gmail.com>
2022-11-04 18:48:54 +0530

    Always use constant file for URLs, don't include them directly
    Constants
    - URL.constant.js

--- frontend/src/Redux/Auth/auth.action.js
+++ frontend/src/Redux/Auth/auth.action.js
@@ -4,6 +4,7 @@ import { AUTH_LOGIN_ERROR, AUTH_LOGIN_LOADING, AUTH_LOGIN_SUCCESS, AUTH_LOGOUT, 
 export const singupAPI = (creds) => async (dispatch) => {
     dispatch({ type: AUTH_LOGIN_LOADING });
     try {
+        // feedback: fw18_0042 - Always use constant file for URLs, don't include them directly
         let res = await axios.post('https://whispering-beyond-98740.herokuapp.com/user/signup', creds);
         // console.log(res.data);
         dispatch({ type: AUTH_SIGNUP_SUCCESS, payload: res.data });
